f441e35aade5ddaacfc5c3fa2f8b4caed61776ef
Noor Azarbeygui <noorazarbeygui@Noors-Air.home>
2022-09-18 21:14:57 -0400

    Initial commit

---
+++ src/Lab_04_03_MonthyCCBalance.java
@@ -0,0 +1,9 @@
+public class Lab_04_03_MonthyCCBalance {
+    public static void main(String[] args) {
+        double balance = 5000;
+        double interest = balance * .17;
+        System.out.println("The Monthly Interest after one month is " + interest);
+        System.out.println("The Monthly Interest after two months is " + interest+balance * 0.17);
+
+    }
+}
